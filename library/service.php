1931a265ddb3de39435b6961030f122a0a576f76
Tsukasa Yukinoshita <vincent@kazamigk.com>
2016-07-04 15:50:30 +0800

    service make up

---
+++ library/service.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 服务类，需要在Bootstrap注册服务组件
+ */
+class service{
+	protected $_components; // 组件实例
+	protected $_definitions; // 组件定义
+	protected static $instance;
+	protected function __construct(){}
+	public static function getInstance(){
+		if (self::$instance) {
+			return self::$instance;
+		}
+		self::$instance = new self;
+		return self::$instance;
+	}
+	public static function __callStatic(string $name, $arguments){
+		return self::getInstance()->fetch($name);
+	}
+	public function __get(string $name){
+		return $this->fetch($name);
+	}
+	public function register(string $name, $definition)
+	{
+		if ( is_string($definition) || is_object($definition) || is_callable($definition, true) ) {
+			$this->_definitions[$name] = $definition;
+		} else {
+			throw new Exception('Invalid definition.');
+		}
+	}
+	
+	public function fetch(string $name)
+	{
+		if ( isset($this->_components[$name]) ) {
+			return $this->_components[$name];
+		}
+		
+		if ( !isset($this->_definitions[$name]) ){
+			return false;
+		}
+
+		if ( is_string($this->_definitions[$name]) && class_exists($this->_definitions[$name]) ) {
+			return $this->_components[$name] = new $this->_definitions[$name];
+		}
+
+		if ( is_callable($this->_definitions[$name], true) ) {
+			return $this->_components[$name] = call_user_func_array($this->_definitions[$name], []);
+		}
+
+		throw new Exception("Undefined service {$name}");
+	}
+
+
+}
+?>
