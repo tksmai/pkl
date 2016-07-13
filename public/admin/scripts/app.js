94ecd31ca5ffd3e77d5add71d68c529d16010c5b
Tsukasa <vincent@kazamigk.com>
2016-07-13 10:47:18 +0800

    13th Jul commit

--- public/admin/scripts/app.js
+++ public/admin/scripts/app.js
@@ -26,7 +26,7 @@ angular
 		$stateProvider
 			.state('admin', {
 				url:'/admin',
-				templateUrl: 'views/dashboard/main.html',
+				templateUrl: 'views/admin/main.html',
 				resolve: {
 						loadMyDirectives:function($ocLazyLoad){
 								return $ocLazyLoad.load(
@@ -77,7 +77,7 @@ angular
 			.state('admin.home',{
 				url:'/home',
 				controller: 'MainCtrl',
-				templateUrl:'views/dashboard/home.html',
+				templateUrl:'views/admin/home.html',
 				resolve: {
 					loadMyFiles:function($ocLazyLoad) {
 						return $ocLazyLoad.load({
@@ -87,17 +87,17 @@ angular
 							'scripts/directives/timeline/timeline.js',
 							'scripts/directives/notifications/notifications.js',
 							'scripts/directives/chat/chat.js',
-							'scripts/directives/dashboard/stats/stats.js'
+							'scripts/directives/admin/stats/stats.js'
 							]
 						})
 					}
 				}
 			})
-			.state('dashboard.form',{
+			.state('admin.form',{
 				templateUrl:'views/form.html',
 				url:'/form'
 		})
-			.state('dashboard.blank',{
+			.state('admin.blank',{
 				templateUrl:'views/pages/blank.html',
 				url:'/blank'
 		})
@@ -105,7 +105,7 @@ angular
 				templateUrl:'views/pages/login.html',
 				url:'/login'
 		})
-			.state('dashboard.chart',{
+			.state('admin.chart',{
 				templateUrl:'views/chart.html',
 				url:'/chart',
 				controller:'ChartCtrl',
@@ -125,31 +125,31 @@ angular
 					}
 				}
 		})
-			.state('dashboard.table',{
+			.state('admin.table',{
 				templateUrl:'views/table.html',
 				url:'/table'
 		})
-			.state('dashboard.panels-wells',{
+			.state('admin.panels-wells',{
 					templateUrl:'views/ui-elements/panels-wells.html',
 					url:'/panels-wells'
 			})
-			.state('dashboard.buttons',{
+			.state('admin.buttons',{
 				templateUrl:'views/ui-elements/buttons.html',
 				url:'/buttons'
 		})
-			.state('dashboard.notifications',{
+			.state('admin.notifications',{
 				templateUrl:'views/ui-elements/notifications.html',
 				url:'/notifications'
 		})
-			.state('dashboard.typography',{
+			.state('admin.typography',{
 			 templateUrl:'views/ui-elements/typography.html',
 			 url:'/typography'
 	 })
-			.state('dashboard.icons',{
+			.state('admin.icons',{
 			 templateUrl:'views/ui-elements/icons.html',
 			 url:'/icons'
 	 })
-			.state('dashboard.grid',{
+			.state('admin.grid',{
 			 templateUrl:'views/ui-elements/grid.html',
 			 url:'/grid'
 	 })
