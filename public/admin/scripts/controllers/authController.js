3b5d5beb4b150e21a647a56b4d3521470d949a2e
Tsukasa <vincent@kazamigk.com>
2016-07-24 21:23:30 +0800

    fix:redirect login page

--- public/admin/scripts/controllers/authController.js
+++ public/admin/scripts/controllers/authController.js
@@ -1,30 +1,30 @@
 angular.module('sbAdminApp')
-  .controller('Auth',['$scope', '$http', '$state', function($scope, $http, $state){
-  	console.log(window.localStorage.sbAdminToken);
-  	var authFlag = false;
-  	var sbAdminToken = window.localStorage.sbAdminToken;
-  	if (sbAdminToken) {
-  		var _httpdata = {
-			url:'index.php/Admin/session',
-			method:'get',
-			timeout:10000,
-			responseType:"json",
-			params:{
-				'device_id':'1',
-				'token':sbAdminToken
+	.controller('Auth',['$scope', '$http', '$state', function($scope, $http, $state){
+		var sbAdminToken = window.localStorage.sbAdminToken;
+		if (sbAdminToken) {
+			var _httpdata = {
+				url:'/index.php/Admin/session',
+				method:'get',
+				timeout:1000,
+				responseType:"json",
+				params:{
+					'device_id':'1',
+					'token':sbAdminToken
+				}
 			}
+			$http(_httpdata).then(function successCallback(response){
+				if (response.status != 200){
+					redirectLogin();
+				}
+			}, function errorCallback(response){
+				// console.log('error request');
+				// console.log(response);
+			});
+		} else {
+			redirectLogin();
 		}
-		$http(_httpdata).then(function successCallback(response){
-			console.log(response);
-			if (response.status === 200){
-				authFlag = true;
-			}
-		}, function errorCallback(response){
-			console.log('error request');
-			console.log(response);
-		});
-  	}
-  	if (authFlag === false){
-  		$state.go('login');
-  	}
-  }]);
+		var redirectLogin = function(){
+			$state.go('login');
+		};
+		
+	}]);
