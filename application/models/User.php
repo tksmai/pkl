faf7832e139c2afe7da5293c937b7733a435f2bc
Tsukasa <vincent@kazamigk.com>
2016-08-06 18:17:18 +0800

    function:check password and fetch user information

--- application/models/User.php
+++ application/models/User.php
@@ -13,9 +13,31 @@ class UserModel extends Model
 				]
 			]
 		];
-		$user_info = $this->select($option);
-		var_dump($this->last_query());
-		var_dump($user_info);
+		$user_info = $this->select($option); // 这里查询得到的是二维数组，待定化成一维数组
+		if ( $user_info ) {
+			$this->user_info = $user_info[0];
+			return true;
+		}
+		return false;
+	}
+
+	public function checkPassword(string $password)
+	{
+		$password = md5($password);
+		@$bool = $password == $this->user_info['password'];
+		return $bool;
+	}
+
+	public function getUserInfo()
+	{
+		if ( !$this->user_info ) {
+			$this->err_msg = 'please check accoutn first!';
+			return false;
+		}
+
+		$user_info = $this->user_info;
+		unset($user_info['password']);
+		return $user_info;
 	}
 }
 ?>
